68d6439b78f2d91391af3a3e869c0bded8391fd5
woosungchu <woosungchu@gmail.com>
2017-07-12 16:59:33 +0900

    add Velocity-animate

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,15 +1,17 @@
 <template>
   <transition 
 	appear 
-	appear-class="slide"
-	appear-to-class="slide-to">
+	v-on:before-appear="beforeAppear"
+  	v-on:appear="appear"
+  	v-on:after-appear="afterAppear"
+  	v-on:appear-cancelled="cancelAppear">
 	  <div id="home-container">
 		<div>
 			<h1 id="name">{{ name }}</h1>
-	  		<h4>{{ msg }}</h4>
+	  		<h4 id="msg">{{ msg }}</h4>
 		</div>
 		<div>
-		  <p>
+		  <p id="links-group">
 			<span v-for="link in links">
 			  <a class="home-link" target="_blank" :href=link.url><i :class=link.icon></i></a> 
 			</span>
@@ -20,6 +22,8 @@
 </template>
 
 <script>
+import Velocity from 'velocity-animate'
+
 export default {
   name: 'home',
   data () {
@@ -32,6 +36,34 @@ export default {
       	{url:'https://stackshare.io/woosungchu/did',icon:'fa fa-angellist fa-2x'}
       ]
     }
+  },
+  methods: {
+    beforeAppear: function (el) {
+      el.querySelector('#name').style.opacity = 0
+      el.querySelector('#msg').style.opacity = 0
+      el.querySelector('#links-group').style.opacity = 0
+    },
+    appear: function (el, done) {
+      var name = el.querySelector('#name'),
+	      msg = el.querySelector('#msg'),
+	      linksGroup = el.querySelector('#links-group');
+      
+      Velocity(name, { opacity: 1, }, { delay: 100 })
+      Velocity(msg, { opacity: 1 }, { delay: 300})
+      Velocity(linksGroup, { opacity: 1 }, { delay: 600})
+      Velocity(el, { fontSize: '1em' }, { delay: 800, complete: done })
+    },
+    afterAppear: function (el, done) {
+    /*
+      Velocity(el, { rotateZ: '50deg' }, { duration: 600 })
+      Velocity(el, { rotateZ: '100deg' }, { loop: 2 })
+      Velocity(el, {
+        rotateZ: '45deg',
+        opacity: 0
+      }, { complete: done })
+    */
+    },
+    cancelAppear: function(){}
   }
 }
 </script>
@@ -54,13 +86,4 @@ export default {
 .home-link{
   color:black !important;
 }
-
-.slide {
-  transform: translateX(-10px);
-  opacity: 0;
-}
-.slide-to {
-  transition: all .3s ease;
-}
-
 </style>
